51d1a699cd5739d0ed9d07710065a550e6e253e8
Riju Ghosh <pg4009355@gmail.com>
2025-04-06 18:53:44 +0530

    Running

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,8 +4,10 @@ import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,24 +2,27 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
-     <div className="container">
-      <div className="mainContiner">
-        <div className="mainContiner_left">
+     <div className={styles.container}>
+      <div className={styles.mainContiner}>
+        <div className={styles.mainContiner_left}>
           <p>Connect with friend</p>
           <p>and the world around you</p>
+
           <div onClick={() => {
-            
+            router.push("/login");
           }} className="buttonJoin">
             <p>Join Now</p>
           </div>
         </div>
-        <div className="mainContiner_right">
+        <div className={styles.mainContiner_right}>
           <img src="image/Register.jpg" alt=""/>
         </div>
       </div>
